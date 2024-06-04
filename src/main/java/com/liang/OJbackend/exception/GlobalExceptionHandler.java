5e88091b2a2307befac412500ee57abf6ab54347
SmartFool <3528268894@qq.com>
2024-06-04 14:49:03 +0800

    commit message

---
+++ src/main/java/com/liang/OJbackend/exception/GlobalExceptionHandler.java
@@ -0,0 +1,30 @@
+package com.liang.OJbackend.exception;
+
+import com.liang.OJbackend.common.BaseResponse;
+import com.liang.OJbackend.common.ErrorCode;
+import com.liang.OJbackend.common.ResultUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+/**
+ * 全局异常处理器
+ *
+  
+ */
+@RestControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(BusinessException.class)
+    public BaseResponse<?> businessExceptionHandler(BusinessException e) {
+        log.error("BusinessException", e);
+        return ResultUtils.error(e.getCode(), e.getMessage());
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public BaseResponse<?> runtimeExceptionHandler(RuntimeException e) {
+        log.error("RuntimeException", e);
+        return ResultUtils.error(ErrorCode.SYSTEM_ERROR, "系统错误");
+    }
+}
---
+++ src/main/java/com/liang/OJbackend/mapper/PostThumbMapper.java
@@ -0,0 +1,17 @@
+package com.liang.OJbackend.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.liang.OJbackend.model.entity.PostThumb;
+
+/**
+ * 帖子点赞数据库操作
+ *
+  
+ */
+public interface PostThumbMapper extends BaseMapper<PostThumb> {
+
+}
+
+
+
+
